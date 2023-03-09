d1e0b7f4c94bd703b1ee969a2c01b2f4a61a901b
Gabriel Santos <sgtux@protonmail.com>
2023-03-09 01:16:30 -0300

    Bash para popular a base de dados, e correção do MD5

--- dvsn/api/src/main/java/br/com/cwi/shop/helpers/StringHelper.java
+++ dvsn/api/src/main/java/br/com/cwi/shop/helpers/StringHelper.java
@@ -44,7 +44,26 @@ public final class StringHelper {
         }
 
         BigInteger hash = new BigInteger(1, md.digest(texto.getBytes()));
-        return hash.toString(16);
+        return leftPad(hash.toString(16), 32, "0");
+    }
+
+    public static String leftPad(String input, int length, String padStr) {
+
+        if(input == null || padStr == null){
+            return null;
+        }
+
+        if(input.length() >= length){
+            return input;
+        }
+
+        int padLength = length - input.length();
+
+        StringBuilder paddedString = new StringBuilder();
+        paddedString.append(padStr.repeat(padLength));
+        paddedString.append(input);
+
+        return paddedString.toString();
     }
 
     public static String pathJoin(String... directories) {
---
+++ dvsn/populate-database.sh
@@ -0,0 +1,2 @@
+psql -h 172.45.45.10 -U postgres -d postgres -a -f ./api/db-script/script.sql
+psql -h 172.45.45.10 -U postgres -d postgres -a -f ./api/db-script/script-rainbow-table.sql
